733db4b1fe29343b5392e6e5e47baed11f1eca4c
Dubois Celine <celine.dubois17@free.fr>
2025-02-15 17:27:10 +0100

    modification de App.vue

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,2 +1,22 @@
-
+<template>
+    <footer>
+      <p>&copy; 15/02/2025. Tous droits réservés.</p>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Footer'
+  }
+  </script>
+  
+  <style scoped>
+  footer {
+    background-color: #black;
+    color: white;
+    padding: 10px;
+    text-align: center;
+  }
+  </style>
+  
   
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,44 +1,46 @@
 <template>
-    <header>
-      <h1>DUBOIS Céline</h1>
-      <nav>
-        <ul>
-          <li><a href="#je-suis">Qui Suis-je ?</a></li>
-          <li><a href="#projets">Mes Projets</a></li>
-          <li><a href="#competences">Mes compétences</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
-    </header>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Header'
-  }
-  </script>
-  
-  <style scoped>
-  header {
-    background-color: #333;
-    color: white;
-    padding: 10px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  
-  nav ul {
-    list-style: none;
-    display: flex;
-    gap: 15px;
-  }
-  
-  nav a {
-    color: white;
-    text-decoration: none;
-  }
-  
-  nav a:hover {
-    text-decoration: underline;
-  }
+  <header>
+    <h1>DUBOIS Céline</h1>
+    <nav>
+      <ul>
+        <li><a href="#je-suis">Qui Suis-je ?</a></li>
+        <li><a href="#projets">Mes Projets</a></li>
+        <li><a href="#competences">Mes compétences</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+header {
+  color: white;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-top: 60px solid black;
+}
+
+nav ul {
+  list-style: none;
+  display: flex;
+  gap: 15px;
+}
+
+nav a {
+  color: white;
+  text-decoration: none;
+}
+
+nav a:hover {
+  text-decoration: underline;
+}
+</style>
+
